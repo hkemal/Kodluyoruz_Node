d299dd70f155ff5b0be9bd5feeab9bf32e1a96ac
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-01-12 17:22:34 +0300

    lec-02-07

---
+++ lecture_02_07_promise/promise.js
@@ -0,0 +1,45 @@
+/* const promise1 = new Promise((resolve, reject) => {
+  resolve("Veriler alindi");
+  //reject("Bağlanti hatasi");
+});
+
+// console.log(promise1);
+
+// promise1.then((value) => console.log(value));
+
+//promise1.catch((error) => console.log(error));
+
+promise1
+  .then((value) => console.log(value))
+  .catch((error) => console.log(error));
+ */
+
+const books = [
+  { name: "Kitap 1", author: "Yazar 1" },
+  { name: "Kitap 2", author: "Yazar 2" },
+  { name: "Kitap 3", author: "Yazar 3" },
+];
+
+const listBooks = () => {
+  books.map((book) => {
+    console.log(book.name);
+  });
+};
+
+const addBook = (newBook) => {
+  const promise1 = new Promise((resolve, reject) => {
+    books.push(newBook);
+    // resolve(books);
+    reject("Bir hata oluştu");
+  });
+  return promise1;
+};
+
+addBook({ name: "Kitap 10", author: "Yazar 10" })
+  .then(() => {
+    console.log("Yeni liste");
+    listBooks();
+  })
+  .catch((error) => {
+    console.log(error);
+  });
