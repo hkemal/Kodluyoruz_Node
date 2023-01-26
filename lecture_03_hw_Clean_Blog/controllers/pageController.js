d6953027f9be86a68ce10f5af503a5a024cca281
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-01-27 00:14:30 +0300

    03-Clean-Blog-hw11

---
+++ lecture_03_hw_Clean_Blog/controllers/pageController.js
@@ -0,0 +1,14 @@
+const Post = require('../models/Post');
+
+exports.aboutPage = (req, res) => {
+  res.render('about');
+};
+
+exports.addPage = (req, res) => {
+  res.render('add_post');
+};
+
+exports.editPage = async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.render('edit', { post });
+};
