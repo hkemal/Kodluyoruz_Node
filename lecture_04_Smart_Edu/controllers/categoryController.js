77e66ab9693daaf0d5dcd42bdb78b477f454746a
kemal <hasankemal.erkaraca@aurorabilisim.com>
2023-01-28 23:23:14 +0300

    04-08-SmartEdu-categories

---
+++ lecture_04_Smart_Edu/controllers/categoryController.js
@@ -0,0 +1,46 @@
+const Category = require('../models/Category');
+
+exports.createCategory = async (req, res) => {
+  try {
+    const category = await Category.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      category: category,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error: error,
+    });
+  }
+};
+
+// exports.getAllCourses = async (req, res) => {
+//   try {
+//     const courses = await Course.find({});
+//     res.status(200).render('courses', {
+//       courses: courses,
+//       page_name: 'courses',
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       status: 'fail',
+//       error: error,
+//     });
+//   }
+// };
+
+// exports.getCourse = async (req, res) => {
+//   try {
+//     const course = await Course.findOne({ slug: req.params.slug });
+//     res.status(200).render('course', {
+//       course: course,
+//       page_name: 'courses',
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       status: 'fail',
+//       error: error,
+//     });
+//   }
+// };
